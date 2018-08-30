4e280626d43010343f7cd20986c8466ecd304979
Joao Meyer <1994meyer@gmail.com>
2018-08-30 19:12:15 -0300

    Created the board of the game.
    The board has some utility functions for getting rows and columns. It
    also has simple set and get functions for the coordinate system of the
    board. The __str__ function return an string representation of the
    board. Good to be used when printing game states.

---
+++ ai/board.py
@@ -0,0 +1,71 @@
+
+
+class Board(object):
+    def __init__(self, width, height):
+        super(Board, self).__init__()
+        self.width = width
+        self.height = height
+        self.board = [ 0 for _ in range(width * height) ]
+
+    def row(self, i):
+        '''
+        Gets row from of board.
+        '''
+        start = i * self.width
+        end = start + self.width
+        return self.board[start:end]
+
+    @property
+    def rows(self):
+        '''
+        Iterator for all the rows in the board.
+        '''
+        for i in range(self.height):
+            yield self.row(i)
+
+    def col(self, i):
+        '''
+        Gets col of board.
+        '''
+        start = i
+        end = (self.width * self.height)
+        step = self.width
+        return self.board[start:end:step]
+
+    @property
+    def cols(self):
+        '''
+        Iterator for all the cols in the board.
+        '''
+        for i in range(self.width):
+            yield self.col(i)
+
+    def at(self, x, y):
+        '''
+        Gets the play at the coordinate.
+
+        Returns:
+            1 for a player, -1 for the other, 0 for nothing.
+        '''
+        return self.board[y * self.width + x]
+
+    def set(self, x, y, val):
+        self.board[y * self.width + x] = val
+
+    def __str__(self):
+        '''
+        Returns string representation of board.
+
+        Note that the representation follow the coordinate system where both
+        minimum values of x and y are located in the lower left corner.
+        '''
+        rows = []
+        for row in self.rows:
+            line = [ str(i).rjust(3) for i in row ]
+            line = ''.join(line)
+            line = line.replace('0', '.')
+            rows.append(line)
+        return '\n'.join(rows[::-1])
+
+    def __len__(self):
+        return len(self.board)
