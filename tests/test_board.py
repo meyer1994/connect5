299c9bfe77a4e8ff2faf70531fd3c12c9c1bfbd8
Joao Meyer <1994meyer@gmail.com>
2018-08-30 19:12:15 -0300

    Created the board of the game.
    The board has some utility functions for getting rows and columns. It
    also has simple set and get functions for the coordinate system of the
    board. The __str__ function return an string representation of the
    board. Good to be used when printing game states.

---
+++ tests/test_board.py
@@ -0,0 +1,78 @@
+from unittest import TestCase
+
+from ai.board import Board
+
+class TestBoard(TestCase):
+
+    def setUp(self):
+        self.board = Board(5, 5)
+        self.board.board = [
+            0, 0, 0, 0,  1,
+            1, 1, 1, 1,  0,
+            0, 0, 0, 0,  0,
+            1, 1, 1, 1, -1,
+            1, 0, 1, 0,  1
+        ]
+
+    def test_constructor(self):
+        self.assertEqual(self.board.width, 5)
+        self.assertEqual(self.board.height, 5)
+
+    def test_len(self):
+        res = len(self.board)
+        self.assertEqual(res, 25)
+
+    def test_row(self):
+        res = self.board.row(0)
+        row = [ 0, 0, 0, 0, 1 ]
+        self.assertListEqual(res, row)
+
+        res = self.board.row(3)
+        row = [ 1, 1, 1, 1, -1 ]
+        self.assertListEqual(res, row)
+
+    def test_rows(self):
+        for i, row in enumerate(self.board.rows):
+            exp = self.board.row(i)
+            self.assertListEqual(exp, row)
+
+    def test_cols(self):
+        for i, col in enumerate(self.board.cols):
+            exp = self.board.col(i)
+            self.assertListEqual(exp, col)
+
+    def test_col(self):
+        res = self.board.col(0)
+        col = [ 0, 1, 0, 1, 1 ]
+        self.assertListEqual(res, col)
+
+        res = self.board.col(3)
+        col = [ 0, 1, 0, 1, 0 ]
+        self.assertListEqual(res, col)
+
+
+    def test_at(self):
+        coords = [ (0, 0), (0, 3), (0, 1), (3, 3) ]
+        results = [ 0, 1, 1, 1 ]
+
+        for coord, res in zip(coords, results):
+            at = self.board.at(*coord)
+            self.assertEqual(at, res)
+
+    def test_set(self):
+        self.board.set(1, 1, -1)
+        res = self.board.at(1, 1)
+        self.assertEqual(res, -1)
+
+    def test_str(self):
+        string = ('  1  .  1  .  1\n'
+                  '  1  1  1  1 -1\n'
+                  '  .  .  .  .  .\n'
+                  '  1  1  1  1  .\n'
+                  '  .  .  .  .  1')
+        res = str(self.board)
+        print(string)
+        print('='*8)
+        print(res)
+        self.assertEqual(string, res)
+
