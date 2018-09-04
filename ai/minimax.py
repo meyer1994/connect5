4e0c93e072601042b5138de723a998961d1868fc
João Meyer <1994meyer@gmail.com>
2018-09-04 17:08:10 -0300

    Still prototipying

--- ai/minimax.py
+++ ai/minimax.py
@@ -2,21 +2,19 @@ from math import inf as INFINITY
 
 from ai.heuristic import evaluate
 
-MAX_DEPTH = 10
-depth = 0
+MAX_DEPTH = 3
+depth = -1
 
 def min_play(game, player):
-    global depth
-    global MAX_DEPTH
-
-    if depth > MAX_DEPTH:
-        return evaluate(game.board)
+    global depth, MAX_DEPTH
     depth += 1
+    if depth >= MAX_DEPTH or game.over:
+        return evaluate(game.board)
 
     moves = game.moves
     best_score = INFINITY
     for x, y in moves:
-        copy = game.next(x, y, player)
+        copy = game.next(x, y, -player)
         score = max_play(copy, player)
         if score < best_score:
             best_score = score
@@ -24,12 +22,10 @@ def min_play(game, player):
 
 
 def max_play(game, player):
-    global depth
-    global MAX_DEPTH
-
-    if depth > MAX_DEPTH:
-        return evaluate(game.board)
+    global depth, MAX_DEPTH
     depth += 1
+    if depth >= MAX_DEPTH or game.over:
+        return evaluate(game.board)
 
     moves = game.moves
     best_score = -INFINITY
@@ -42,8 +38,11 @@ def max_play(game, player):
 
 
 def minimax(game, player):
-    moves = game.moves
-    best_move = next(moves)
+    global depth
+    depth = -1
+
+    moves = list(game.moves)
+    best_move = moves[0]
     best_score = -INFINITY
     for x, y in moves:
         copy = game.next(x, y, player)
