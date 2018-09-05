b72040380b2859c7d8d81ac3967809de022c6840
João Meyer <1994meyer@gmail.com>
2018-09-05 13:05:13 -0300

    Fixed the minimax function.
    Now it returns correct values.

--- ai/minimax.py
+++ ai/minimax.py
@@ -2,51 +2,39 @@ from math import inf as INFINITY
 
 from ai.heuristic import evaluate
 
-MAX_DEPTH = 3
+MAX_DEPTH = 50
 depth = -1
 
-def min_play(game, player):
+def min_play(game):
     global depth, MAX_DEPTH
     depth += 1
     if depth >= MAX_DEPTH or game.over:
         return evaluate(game.board)
 
-    moves = game.moves
-    best_score = INFINITY
-    for x, y in moves:
-        copy = game.next(x, y, -player)
-        score = max_play(copy, player)
-        if score < best_score:
-            best_score = score
-    return best_score
+    games = ( game.next(x, y, -1) for x, y in game.moves )
+    scores = map(max_play, games)
+    return min(scores)
 
-
-def max_play(game, player):
+def max_play(game):
     global depth, MAX_DEPTH
     depth += 1
     if depth >= MAX_DEPTH or game.over:
         return evaluate(game.board)
 
-    moves = game.moves
-    best_score = -INFINITY
-    for x, y in moves:
-        copy = game.next(x, y, player)
-        score = min_play(copy, player)
-        if score > best_score:
-            best_score = score
-    return best_score
+    games = ( game.next(x, y, 1) for x, y in game.moves )
+    scores = map(min_play, games)
+    return max(scores)
 
 
-def minimax(game, player):
+def minimax(game):
     global depth
-    depth = -1
 
-    moves = list(game.moves)
-    best_move = moves[0]
+    best_move = None
     best_score = -INFINITY
-    for x, y in moves:
-        copy = game.next(x, y, player)
-        score = min_play(copy, player)
+    for x, y in game.moves:
+        depth = -1
+        copy = game.next(x, y, 1)
+        score = min_play(copy)
         if score > best_score:
             best_move = (x, y)
             best_score = score
