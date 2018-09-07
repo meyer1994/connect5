aa05042073917f0264d7e07d7a74b41033dfbac9
Joao Meyer <1994meyer@gmail.com>
2018-09-07 15:50:58 -0300

    Changed board to use strings

--- tests/test_board.py
+++ tests/test_board.py
@@ -6,13 +6,13 @@ class TestBoard(TestCase):
 
     def setUp(self):
         self.board = Board(5, 5)
-        self.board.board = [
-            0, 0, 0, 0,  1,
-            1, 1, 1, 1,  0,
-            0, 0, 0, 0,  0,
-            1, 1, 1, 1, -1,
-            1, 0, 1, 0,  1
-        ]
+        self.board.board = ''.join([
+            '----X',
+            'XXXX-',
+            '-----',
+            'XXXXO',
+            'X-X-X'
+        ])
         # X - X - X
         # X X X X O
         # - - - - -
@@ -25,101 +25,101 @@ class TestBoard(TestCase):
 
     def test_row(self):
         res = self.board.row(0)
-        row = [ 0, 0, 0, 0, 1 ]
-        self.assertListEqual(res, row)
+        print(type(self.board.board))
+        row = '----X'
+        self.assertEqual(res, row)
 
         res = self.board.row(3)
-        row = [ 1, 1, 1, 1, -1 ]
-        self.assertListEqual(res, row)
+        row = 'XXXXO'
+        self.assertEqual(res, row)
 
     def test_rows(self):
         for i, row in enumerate(self.board.rows):
             exp = self.board.row(i)
-            self.assertListEqual(exp, row)
+            self.assertEqual(exp, row)
 
     def test_cols(self):
         for i, col in enumerate(self.board.cols):
             exp = self.board.col(i)
-            self.assertListEqual(exp, col)
+            self.assertEqual(exp, col)
 
     def test_col(self):
         res = self.board.col(0)
-        col = [ 0, 1, 0, 1, 1 ]
-        self.assertListEqual(res, col)
+        col = '-X-XX'
+        self.assertEqual(res, col)
 
         res = self.board.col(3)
-        col = [ 0, 1, 0, 1, 0 ]
-        self.assertListEqual(res, col)
+        col = '-X-X-'
+        self.assertEqual(res, col)
 
     def test_rdiag(self):
         expected = [
-            [ 1 ],
-            [ 0, 1 ],
-            [ 1, 1, 0 ],
-            [ 0, 1, 0, 1 ],
-            [ 1, 1, 0, 1, 0 ],
-            [ -1, 0, 1, 0 ],
-            [ 0, 1, 0 ],
-            [ 0, 0 ],
-            [ 1 ]
+            'X',
+            'X-',
+            '-XX',
+            'X-X-',
+            '-X-XX',
+            '-X-O',
+            '-X-',
+            '--',
+            'X'
         ]
-        for i, exp in enumerate(expected):
-            res = self.board.rdiag(i)
-            self.assertListEqual(res, exp)
+        for diag, exp in enumerate(expected):
+            res = self.board.rdiag(diag)
+            print(res)
+            self.assertEqual(res, exp)
 
     def test_ldiag(self):
         expected = [
-            [ 0 ],
-            [ 1, 0 ],
-            [ 0, 1, 0 ],
-            [ 1, 0, 1, 0 ],
-            [ 1, 1, 0, 1, 1 ],
-            [ 0, 1, 0, 0 ],
-            [ 1, 1, 0 ],
-            [ 0, -1 ],
-            [ 1 ]
+            '-',
+            'X-',
+            '-X-',
+            'X-X-',
+            'XX-XX',
+            '-X--',
+            'XX-',
+            '-O',
+            'X'
         ]
+
         for i, exp in enumerate(expected):
             res = self.board.ldiag(i)
-            self.assertListEqual(res, exp)
+            self.assertEqual(res, exp)
 
     def test_diags(self):
         expected = [
-            [ 1 ],
-            [ 0, 1 ],
-            [ 1, 1, 0 ],
-            [ 0, 1, 0, 1 ],
-            [ 1, 1, 0, 1, 0 ],
-            [ -1, 0, 1, 0 ],
-            [ 0, 1, 0 ],
-            [ 0, 0 ],
-            [ 1 ],
-            [ 0 ],
-            [ 1, 0 ],
-            [ 0, 1, 0 ],
-            [ 1, 0, 1, 0 ],
-            [ 1, 1, 0, 1, 1 ],
-            [ 0, 1, 0, 0 ],
-            [ 1, 1, 0 ],
-            [ 0, -1 ],
-            [ 1 ]
+            'X',
+            'X-',
+            '-XX',
+            'X-X-',
+            '-X-XX',
+            '-X-O',
+            '-X-',
+            '--',
+            'X',
+            '-',
+            'X-',
+            '-X-',
+            'X-X-',
+            'XX-XX',
+            '-X--',
+            'XX-',
+            '-O',
+            'X'
         ]
-        diags = self.board.diags
-        for res, exp in zip(diags, expected):
-            self.assertListEqual(res, exp)
+        results = list(self.board.diags)
+        self.assertListEqual(results, expected)
 
     def test_get(self):
         coords = [ (0, 0), (0, 3), (0, 1), (3, 3) ]
-        results = [ 0, 1, 1, 1 ]
-
-        for coord, res in zip(coords, results):
-            at = self.board.get(*coord)
-            self.assertEqual(at, res)
+        expected = [ '-', 'X', 'X', 'X' ]
+        results = [ self.board.get(x, y) for x, y in coords ]
+        self.assertListEqual(expected, results)
 
     def test_set(self):
-        self.board.set(1, 1, -1)
+        self.board.set(1, 1, 'O')
         res = self.board.get(1, 1)
-        self.assertEqual(res, -1)
+        self.assertEqual(res, 'O')
 
     def test_str(self):
         string = ('X - X - X\n'
@@ -129,7 +129,22 @@ class TestBoard(TestCase):
                   '- - - - X')
         res = str(self.board)
         self.assertEqual(string, res)
+        res = repr(self.board)
+        self.assertEqual(string, res)
 
     def test_len(self):
         res = len(self.board)
         self.assertEqual(res, 25)
+
+    def test_eq(self):
+        b1 = Board(20, 20)
+        b2 = Board(20, 20)
+        self.assertEqual(b1, b2)
+
+        b1.set(0, 0, 'x')
+        self.assertNotEqual(b1, b2)
+
+    def test_getitem(self):
+        expected = '----X'
+        result = self.board[0:5]
+        self.assertEqual(expected, result)
