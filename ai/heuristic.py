9cbeadf5159d9fef43d3ed8e0ae03f2ef32cdf86
Joao Meyer <1994meyer@gmail.com>
2018-09-07 17:07:15 -0300

    Simplified the heuristic module

--- ai/heuristic.py
+++ ai/heuristic.py
@@ -3,9 +3,11 @@ from collections import defaultdict
 
 ONLY_X = { ''.join(k): 3**k.count('X') for k in product('X-', repeat=5) }
 ONLY_X['XXXXX'] = 3**14
+del ONLY_X['-----']
 
 ONLY_O = { ''.join(k): -3**k.count('O') for k in product('O-', repeat=5) }
 ONLY_O['OOOOO'] = -3**14
+del ONLY_O['-----']
 
 VALUE_TABLE = defaultdict(lambda: 0)
 VALUE_TABLE.update(ONLY_X)
@@ -13,28 +15,7 @@ VALUE_TABLE.update(ONLY_O)
 VALUE_TABLE['-----'] = 0
 
 
-def is_open(line):
-    '''
-    Checks if a line is open.
-
-    An open line is a line of length 5 in which there is only pieces of one
-    player.
-
-    Args:
-        line: line to check.
-
-    Returns:
-        True if it is open, False otherwise.
-    '''
-    global ONLY_O, ONLY_X
-
-    if line == '-----':
-        return False
-    return line in ONLY_O or line in ONLY_X
-
-
 def evaluate_line(line):
-    global VALUE_TABLE
     return VALUE_TABLE[line]
 
 
@@ -60,7 +41,6 @@ def evaluate(game):
         l = len(line) - 4
         for i in range(l):
             sub_line = line[i:i+5]
-            if is_open(sub_line):
-                result += evaluate_line(sub_line)
+            result += evaluate_line(sub_line)
 
     return result / plays
